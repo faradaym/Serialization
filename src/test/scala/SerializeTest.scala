02d468f7a589fa4620835e6c05d001ff34152c7a
faraday <rcheyenne.truss@gmail.com>
2019-02-12 01:48:49 -0600

    unit tests

--- src/test/scala/SerializeTest.scala
+++ src/test/scala/SerializeTest.scala
@@ -25,13 +25,13 @@ class HtmlTest extends FlatSpec{
     val x: String = "peep"
     val y: Double = 3.0
     val z: Char = 'r'
-    assert(convertToJson(x) == "peep", "strings")
-    assert(convertToJson(y) == "3.0", "nums")
-    assert(convertToJson(z) == "r", "chars")
+    assert(convertToHtml(x) == "peep", "strings")
+    assert(convertToHtml(y) == "3.0", "nums")
+    assert(convertToHtml(z) == "r", "chars")
   }
   it should "serialize linear collections to ordered lists" in {
     val lst = 1.3 :: 2.4 ::  Nil
-    assert(convertToJson(lst) == "<ol><li>1.3</li><li>2.4</li></ol>", "homogeneous list")
+    assert(convertToHtml(lst) == "<ol><li>1.3</li><li>2.4</li></ol>", "homogeneous list")
   }
   it should "serialize associative collections to definition lists" in{
 
